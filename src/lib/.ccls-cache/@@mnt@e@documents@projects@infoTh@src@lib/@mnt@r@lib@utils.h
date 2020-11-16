b8ca619775bf31610aa1e7696e3cb5e999fe93bc
SdtElectronics <meiyang423@Gmail.com>
2020-11-16 19:13:11 +0800

    remove ccls cache

--- src/lib/.ccls-cache/@@mnt@e@documents@projects@infoTh@src@lib/@mnt@r@lib@utils.h
+++
@@ -1,7 +0,0 @@
-#include <utility>
-
-inline std::pair<size_t, size_t> taskDiv(size_t nThreads, size_t taskSize){	
-	size_t lastChunkSize = taskSize%nThreads;
-	size_t chunkSize = (taskSize - lastChunkSize)/nThreads;
-	return std::pair<size_t, size_t>(chunkSize, lastChunkSize + chunkSize);
-}
