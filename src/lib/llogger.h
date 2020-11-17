c768535e45408c492857c92ab17f9f9e1a15f3a3
SdtElectronics <meiyang423@Gmail.com>
2020-11-17 22:58:42 +0800

    add llogger

---
+++ src/lib/llogger.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class llogger{
+    public:
+        enum level: char{all = 0, verbose, message, warning, error, silent};
+
+        llogger(std::ostream &os, level lev);
+        template <typename T>
+        llogger& operator << (const T &content);
+        llogger& operator << (level content);
+
+    private : 
+        std::ostream &_os;
+        level _level;
+        level curLev;
+};
